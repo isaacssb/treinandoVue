b28e0c6c499bf4c60310c002db784999ff9cb25b
Isaac Barreto <isaacssb123@gmail.com>
2022-07-08 22:33:23 -0300

    Diretivas

--- primeroProjeto/src/components/Pessoa.vue
+++ primeroProjeto/src/components/Pessoa.vue
@@ -16,7 +16,7 @@
     },
     data() {
       return{
-        nome: 'Matheus',
+        nome: 'Isaac',
       }
     }
   }
--- primeroProjeto/src/components/info.vue
+++ primeroProjeto/src/components/info.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
-    <p>Estou trabalhando</p>
-    <p>Me chama depois</p>
-    <p>Se pá nem me chama</p>
+    <p v-if="esta_trabalhando">TRABALHANDO</p>
+    <p v-else="esta_trabalhando">DESEMPREGADO</p>
     <ul>
-      <li>teste</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
     </ul>
+    <p v-show="mostrar_email">Mende uma mensagem para : {{ email }}</p>
   </div>
 </template>
-<script>
-
+<script lang='ts'>
 export default {
-
-  name: 'Info'
+  name: 'Info',
+  data() {
+    return{
+      esta_trabalhando: false,
+      mostrar_email: true,
+      email: 'isaacssb@gmail.com'
+    }
+  }
 }
 </script>
