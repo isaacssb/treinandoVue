a1990ef57ac330d863645ec542d95b560a4742fa
Isaac Barreto <isaacssb123@gmail.com>
2022-07-08 23:06:54 -0300

    v-bind

--- primeroProjeto/src/components/info.vue
+++ primeroProjeto/src/components/info.vue
@@ -8,16 +8,23 @@
       <li>3</li>
     </ul>
     <p v-show="mostrar_email">Mende uma mensagem para : {{ email }}</p>
+    <p>Acesse o link <a v-bind:href="link" target="_blank">aqui</a></p>
+    <Picture />
   </div>
 </template>
-<script lang='ts'>
+<script>
+import Picture from "./Picture.vue"
 export default {
   name: 'Info',
+  components: {
+    Picture
+  },
   data() {
     return{
       esta_trabalhando: false,
       mostrar_email: true,
-      email: 'isaacssb@gmail.com'
+      email: 'isaacssb@gmail.com',
+      link: 'https://google.com'
     }
   }
 }
