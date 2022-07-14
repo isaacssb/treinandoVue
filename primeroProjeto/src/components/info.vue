e9a8fbb210edfca0fd4d10126f23585c9d8fba3a
Isaac Barreto <isaacssb123@gmail.com>
2022-07-13 23:57:18 -0300

    Entendimento sobre v-for

--- primeroProjeto/src/components/info.vue
+++ primeroProjeto/src/components/info.vue
@@ -3,9 +3,13 @@
     <p v-if="esta_trabalhando">TRABALHANDO</p>
     <p v-else="esta_trabalhando">DESEMPREGADO</p>
     <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
+      <li v-for="(technology, index) in backend_technologies" v-bind:key="index">{{technology}}</li>
+    </ul>
+    <p>List front end</p>
+    <ul>
+      <li v-for="technology in frontend_technologies">
+        {{technology.language}}
+      </li>
     </ul>
     <p v-show="mostrar_email">Mende uma mensagem para : {{ email }}</p>
     <p>Acesse o link <a v-bind:href="link" target="_blank">aqui</a></p>
@@ -24,7 +28,13 @@ export default {
       esta_trabalhando: false,
       mostrar_email: true,
       email: 'isaacssb@gmail.com',
-      link: 'https://google.com'
+      link: 'https://google.com',
+      backend_technologies: ["JS","PHP","Phython"],
+      frontend_technologies: [
+        {id: 1, language: 'HTML'},
+        {id: 2, language: 'CSS'},
+        {id: 3, language: 'VUE'}
+      ]
     }
   }
 }
